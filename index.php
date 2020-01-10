44c4d348e20ad584bed9e5be4b9dec4159d2018a
Meret D <meret.danatarov@yandex.ru>
2020-01-10 15:16:56 +0300

    portfolio first commit

---
+++ index.php
@@ -0,0 +1,294 @@
+<?php
+    define('SITE_KEY', '6Lc-H8kUAAAAAHv676hqeo8O_wbQXf0ZyTJd9HvO');
+    define('SECRET_KEY', '6Lc-H8kUAAAAABfpFEq6JZI5MGOcKLHW5PWV9f66');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>MPro-Professional Solutions</title>
+  <!-- MDB icon -->
+  <link rel="icon" href="img/mpro_favicon.ico" type="image/x-icon">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <!-- Material Design Bootstrap -->
+  <link rel="stylesheet" href="css/mdb.min.css">
+  <!-- Your custom styles (optional) -->
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+  <!-- Start your project here-->  
+  <div style="">
+    <div class="">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-6 col-12">
+            <img class="w-100" src="img/programming2.gif" alt="">
+          </div>
+          <div class="col-12 px-4 pb-4 pt-1">
+            <h1 class="lead text-center">We are very busy creating awesome staff for <b>You</b> <br/>and don't have time for our website :(</h1>
+            <hr/>
+            <p class="mb-0 text-center"><b>Contact Us: m(at)denet(dot)pro</b></p>
+            <hr/>
+            <p class="lead"><b>Our Works:</b></p>
+            <div class="our-works">
+              <div class="row">
+                <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-4">
+                  <div class="ow-item">
+                    <img src="img/works/inashgabat.jpg" alt="">
+                    <a href="http://ashgabat.in/en/" class="ow-link" target="_blank">
+                      <i class="fas fa-globe"></i>
+                      <i class="fab fa-wordpress"></i>
+                      <span>ashgabat.in</span>
+                    </a>
+                  </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-4">
+                  <div class="ow-item">
+                    <img src="img/works/carstoris.jpg " alt="">
+                    <a href="https://carstoris.ru/" class="ow-link" target="_blank">
+                      <i class="fas fa-globe"></i>
+                      <i class="fab fa-symfony"></i>
+                      <span>carstoris.ru</span>
+                    </a>
+                  </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-4">
+                  <div class="ow-item">
+                    <img src="img/works/vizitka.jpg " alt="">
+                    <a href="http://vizitka.test-onpromo.ru/" class="ow-link" target="_blank">
+                      <i class="fas fa-globe"></i>
+                      <i class="fab fa-symfony"></i>
+                      <span>vizitka.onpromo.ru</span>
+                    </a>
+                  </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-4">
+                  <div class="ow-item">
+                    <img src="img/works/beauty-shkaf.jpg " alt="">
+                    <a href="https://beautyshkaf.shop/" class="ow-link" target="_blank">
+                      <i class="fas fa-shopping-basket"></i>
+                      <!-- <i class="fas fa-globe"></i> -->
+                      <i class="fab fa-wordpress"></i>
+                      <span>beautyshkaf.shop</span>
+                    </a>
+                  </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-4">
+                  <div class="ow-item">
+                    <img src="img/works/denet.jpg " alt="">
+                    <a href="https://denet.pro/" class="ow-link" target="_blank">
+                      <i class="fas fa-globe"></i>
+                      <i class="fab fa-wordpress"></i>
+                      <span>denet.pro</span>
+                    </a>
+                  </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-4">
+                  <div class="ow-item">
+                    <img src="img/works/storage.denet.jpg " alt="">
+                    <a href="https://storage.denet.pro/" class="ow-link" target="_blank">
+                      <i class="fas fa-globe"></i>
+                      <i class="fab fa-vuejs"></i>
+                      <span>storage.denet.pro</span>
+                    </a>
+                  </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-4">
+                  <div class="ow-item">
+                    <img src="img/works/barer-promo.jpg " alt="">
+                    <a href="https://www.xn--80abn2ajbegbb2k.xn--p1ai/" class="ow-link" target="_blank">
+                      <i class="fas fa-globe"></i>
+                      <!-- <i class="fab fa-wordpress"></i> -->
+                      <span>барьерпромо.рф</span>
+                    </a>
+                  </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-4">
+                  <div class="ow-item">
+                    <img src="img/works/fbbs.jpg " alt="">
+                    <a href="https://fatih.sch.id/" class="ow-link" target="_blank">
+                      <i class="fas fa-globe"></i>
+                      <i class="fab fa-wordpress"></i>
+                      <span>fatih.sch.id</span>
+                    </a>
+                  </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-4">
+                  <div class="ow-item">
+                    <img src="img/works/giras-sp.jpg " alt="">
+                    <a href="https://giras-sp.ru/" class="ow-link" target="_blank">
+                      <!-- <i class="fas fa-globe"></i> -->
+                      <i class="fas fa-shopping-basket"></i>
+                      <i class="fab fa-opencart"></i>
+                      <span>giras-sp.ru</span>
+                    </a>
+                  </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-4">
+                  <div class="ow-item">
+                    <img src="img/works/nailh.jpg " alt="">
+                    <a href="https://nailh.ru/" class="ow-link" target="_blank">
+                      <!-- <i class="fas fa-globe"></i> -->
+                      <i class="fas fa-shopping-basket"></i>
+                      <i class="fab fa-opencart"></i>
+                      <span>nailh.ru</span>
+                    </a>
+                  </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-4">
+                  <div class="ow-item">
+                    <img src="img/works/group-escort.jpg " alt="">
+                    <a href="https://www.fmeter.ru/esp/promo/fn/" class="ow-link" target="_blank">
+                      <i class="fas fa-globe"></i>
+                      <span>fmeter.ru</span>
+                    </a>
+                  </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-4">
+                  <div class="ow-item">
+                    <img src="img/works/peregorodki.jpg " alt="">
+                    <a href="https://peregorodki.msk.ru/" class="ow-link" target="_blank">
+                      <i class="fas fa-globe"></i>
+                      <i class="fab fa-wordpress"></i>
+                      <span>peregorodki.msk.ru</span>
+                    </a>
+                  </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-4">
+                  <div class="ow-item">
+                    <img src="img/works/blicm.jpg " alt="">
+                    <a href="https://blicm.ru/" class="ow-link" target="_blank">
+                      <i class="fas fa-globe"></i>
+                      <i class="fab fa-wordpress"></i>
+                      <span>blicm.ru</span>
+                    </a>
+                  </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-4">
+                  <div class="ow-item">
+                    <img src="img/works/kharisma.jpg " alt="">
+                    <a href="http://www.kharismabangsa.sch.id/" class="ow-link" target="_blank">
+                      <i class="fas fa-globe"></i>
+                      <i class="fab fa-joomla"></i>
+                      <span>kharismabangsa</span>
+                    </a>
+                  </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-4">
+                  <div class="ow-item">
+                    <img src="img/works/tipografy-pro.jpg " alt="">
+                    <a href="https://tipografy-pro.ru/" class="ow-link" target="_blank">
+                      <i class="fas fa-globe"></i>
+                      <i class="fab fa-wordpress"></i>
+                      <span>tipografy-pro.ru</span>
+                    </a>
+                  </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-4">
+                  <div class="ow-item">
+                    <img src="img/works/etalon.jpg " alt="">
+                    <a href="http://etalon-srv.ru/" class="ow-link" target="_blank">
+                      <i class="fas fa-globe"></i>
+                      <i class="fab fa-wordpress"></i>
+                      <span>etalon-srv.ru</span>
+                    </a>
+                  </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-4">
+                  <div class="ow-item">
+                    <img src="img/works/smaservice.jpg " alt="">
+                    <a href="http://smaservice-kzn.ru/" class="ow-link" target="_blank">
+                      <i class="fas fa-globe"></i>
+                      <i class="fab fa-wordpress"></i>
+                      <span>smaservice-kzn.ru</span>
+                    </a>
+                  </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-4">
+                  <div class="ow-item">
+                    <img src="img/works/sknewlife.jpg " alt="">
+                    <a href="http://www.xn--80asbejw0a4ds.xn--p1ai/" class="ow-link" target="_blank">
+                      <i class="fas fa-globe"></i>
+                      <span>скньюлайф.рф</span>
+                    </a>
+                  </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-4">
+                  <div class="ow-item">
+                    <img src="img/works/rosavtador.jpg " alt="">
+                    <a href="http://vv-fad.ru/new_web/" class="ow-link" target="_blank">
+                      <i class="fas fa-globe"></i>
+                      <i class="fab fa-wordpress"></i>
+                      <span>vv-fad.ru</span>
+                    </a>
+                  </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-4">
+                  <div class="ow-item">
+                    <img src="img/works/a2plus.jpg " alt="">
+                    <a href="https://a2plus.info/" class="ow-link" target="_blank">
+                      <i class="fas fa-globe"></i>
+                      <i class="fab fa-wordpress"></i>
+                      <span>a2plus.info</span>
+                    </a>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-12 text-center logo px-5">
+            <!-- <?php echo file_get_contents('img/logo-mpro.svg'); ?> -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- End your project here-->
+
+  <!-- jQuery -->
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="js/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="js/mdb.min.js"></script>
+  <!-- Your custom scripts (optional) -->
+  <script type="text/javascript"></script>
+  <script src="https://www.google.com/recaptcha/api.js?render=<?php echo SITE_KEY; ?>"></script>
+  <script>
+    grecaptcha.ready(function() {
+      grecaptcha.execute('<?php echo SITE_KEY; ?>', {action: 'homepage'}).then(function(token) {
+        $('#g-recaptcha-response').val(token);
+        // console.log(token);
+      });
+    });
+  </script>
+</body>
+</html>
